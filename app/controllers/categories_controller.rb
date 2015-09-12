a6087d2c5edba763174351dfb04649bce9de32bd
David Garber <davidgarber@gmail.com>
2015-09-12 16:23:46 -0700

    Create categories controller create.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,4 +10,19 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :description)
+  end
 end
