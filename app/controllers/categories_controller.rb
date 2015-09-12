1129a4097b2aa4f05b4e90dfaa5eda95eaa14642
David Garber <davidgarber@gmail.com>
2015-09-12 16:28:18 -0700

    Create controller categories update.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,15 @@ class CategoriesController < ApplicationController
     @category = Category.find(params[:id])
   end
 
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def category_params
